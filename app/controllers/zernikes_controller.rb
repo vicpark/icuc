b4c2c5943cc979d2f9a8e126ec94f26c0678d489
Vlad <vladenso@gmail.com>
2017-11-19 23:17:55 +0000

    fixed # of zernikes issue and flash issue

--- app/controllers/zernikes_controller.rb
+++ app/controllers/zernikes_controller.rb
@@ -61,20 +61,20 @@ class ZernikesController < ApplicationController
         
         #flash[:notice] = zernikes.to_s + parameters.to_s + options.to_s
         # to run matlab code. 
-        #@files = ApplicationHelper.compute(zernikes, parameters, options)
+        @files = ApplicationHelper.compute(zernikes, parameters, options)
         
         #flash[:notice] =  zernikes.length.to_s + " " +parameters.length.to_s + " " + options.length.to_s 
-        system("ls app/assets/images/computed* > app/assets/list.txt")
-        files = []
-        f = File.open("app/assets/list.txt", "r")
+        #system("ls app/assets/images/computed* > app/assets/list.txt")
+        #iles = []
+        #f = File.open("app/assets/list.txt", "r")
         
-        f.each_line do |line|
-            line =~ /(compute.*jpg)/
-            if $1 != nil
-                files << $1
-            end
-        end
-        @files = files
+        #f.each_line do |line|
+        #    line =~ /(compute.*jpg)/
+        #    if $1 != nil
+        #        files << $1
+        #    end
+        #end
+        #@files = files
     end
     
     #Upload action ensures that submitted file is uploaded if it meets the requirements
