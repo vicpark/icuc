1cbfcac07b40183f6c8f412b18a750c87c859067
EricChengX <eric_cheng31@berkeley.edu>
2017-11-20 20:42:45 -0800

    hidden loading

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -1,4 +1,10 @@
-setTimeout(function(){
-  $('body').addClass('loaded');
-  $('h1').css('color', '#222');
-}, 100);
+// setTimeout(function(){
+//   $('body').addClass('loaded');
+//   $('h1').css('color', '#222');
+// }, 100);
+
+$(document).ready(function() {
+    $('#demo').click(function() {
+        $('#slow_warning').show();
+    });
+})
