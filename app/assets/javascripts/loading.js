f5f9d3ebce4f66a38adf9025cfc3928a3d7c1359
EricChengX <eric_cheng31@berkeley.edu>
2017-11-20 20:51:17 -0800

    hidden loading

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -6,5 +6,10 @@
 $(document).ready(function() {
     $('#demo').click(function() {
         $('#slow_warning').show();
+        $('#slower_warning').show();
+        setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color', '#222');
+        }, 100);
     });
 })
