5160baacf22ac4dc3e060304b20b044fb9838dbe
EricChengX <eric_cheng31@berkeley.edu>
2017-12-01 09:59:20 -0800

    Delete autocheckbox.js

--- app/assets/javascripts/autocheckbox.js
+++
@@ -1,6 +0,0 @@
-var chk1 = $("input[type='checkbox'][name = 'options[Convolution for 20 sized letter]']");
-var chk2 = $("input[type='checkbox'][name='options[PSF]']");
-
-chk1.on('change', function(){
-  chk2.prop('checked',this.checked);
-});
