6f369d37d859fddcbe16f0888a5e52d523e15020
WebDesign <webdesign@UbuntuServer.berkeley.edu>
2018-03-30 11:03:04 -0700

    currently working code

--- wave.java
+++ wave.java
@@ -15,45 +15,32 @@ class wave
   
       try
       {
-        wave = new wavreq();
-        String fileid = args[0];
-        // ZERNIKE COEFFICIENTS
-        float zernikes[] = new float[65];
-        for (int i = 0; i < 65; i++)
-            zernikes[i] = Float.parseFloat(args[i+1]); 
-        MWNumericArray coeffs = new MWNumericArray(zernikes, MWClassID.DOUBLE);
-        
-        // other parameters
-        float pupilfit = Float.parseFloat(args[66]);
-        float pupilcalc = Float.parseFloat(args[67]);
-        float defocus = Float.parseFloat(args[68]);
-        float wavelength = Float.parseFloat(args[69]);
-        float pixels = Float.parseFloat(args[70]);
-        float pupilfieldsize = Float.parseFloat(args[71]);
-        float lettersize = Float.parseFloat(args[72]); 
-        
-        // testing defocus range
-        float defocusRange[] = new float[3];
-        defocusRange[0] = Float.parseFloat("1");
-        defocusRange[1] = Float.parseFloat("2");
-        defocusRange[2] = 5.0;
-        MWNumericArray defocusRanges = new MWNumericArray(defocusRange, MWClassID.DOUBLE);
-        
-        // choice of graphs
-        int WF = Integer.parseInt(args[73]);
-        int PSF = Integer.parseInt(args[74]);
-        int MTF = Integer.parseInt(args[75]);
-        int PTF = Integer.parseInt(args[76]);
-        int MTFL = Integer.parseInt(args[77]);
-        int CONV = Integer.parseInt(args[78]);
-        
-        // result fits
-        result = wave.WaveReq(2, fileid, coeffs, pupilfit, pupilcalc, defocusRanges, wavelength, pixels, pupilfieldsize, lettersize, WF, PSF, MTF, PTF, MTFL, CONV);
-        System.out.println(result);
+         wave = new wavreq();
+         String fileid = args[0];
+	 float zernikes[] = new float[65];
+	 for (int i = 0; i < 65; i++)
+		zernikes[i] = Float.parseFloat(args[i+1]); 
+	 MWNumericArray coeffs = new MWNumericArray(zernikes, MWClassID.DOUBLE);
+         float pupilfit = Float.parseFloat(args[66]);
+         float pupilcalc = Float.parseFloat(args[67]);
+         float defocus = Float.parseFloat(args[68]);
+         float wavelength = Float.parseFloat(args[69]);
+         float pixels = Float.parseFloat(args[70]);
+         float pupilfieldsize = Float.parseFloat(args[71]);
+         float lettersize = Float.parseFloat(args[72]); 
+
+	 int WF = Integer.parseInt(args[73]);
+	 int PSF = Integer.parseInt(args[74]);
+	 int MTF = Integer.parseInt(args[75]);
+	 int PTF = Integer.parseInt(args[76]);
+	 int MTFL = Integer.parseInt(args[77]);
+	 int CONV = Integer.parseInt(args[78]);
+	 result = wave.WaveReq(2, fileid, coeffs, pupilfit, pupilcalc, defocus, wavelength, pixels, pupilfieldsize, lettersize, WF, PSF, MTF, PTF, MTFL, CONV);
+         System.out.println(result);
       }
       catch (Exception e)
       {
-        System.out.println("Exception: " + e.toString());
+         System.out.println("Exception: " + e.toString());
       }
       finally
       {
